7627c8447633c7a1f885a90db5dd80635ce5c68c
moggishere <ahro_kr3@hotmail.com>
2024-04-29 09:32:24 -0300

    (chore) adicionado story de Button

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import {
+  Button as ButtonComponent,
+  buttonVariants,
+} from "@/components/ui/button";
+
+const meta = {
+  title: "Example/Button",
+  component: ButtonComponent,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof ButtonComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: function Component(args) {
+    return (
+      <ButtonComponent className={buttonVariants()}>Clica eu</ButtonComponent>
+    );
+  },
+};
+
+export const Destructive: Story = {
+  render: () => (
+    <ButtonComponent variant="destructive">Button destrutivo</ButtonComponent>
+  ),
+};
